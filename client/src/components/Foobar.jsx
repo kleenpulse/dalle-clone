c359c3e40205856770d388071df5d6bbfbac37e8
kleenpulse <skyeparagon@gmail.com>
2023-01-26 15:17:34 +0100

    feat: added foo bar

---
+++ client/src/components/Foobar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Foobar = () => {
+	return (
+		<div className="mt-10 bottom-0 flex justify-center w-f items-center">
+			Made with 🖤 Liquid | {new Date().getFullYear()}
+		</div>
+	);
+};
+
+export default Foobar;
