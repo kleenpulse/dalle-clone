b82261856cf26a73d4b9eec6efe3827160347148
kleenpulse <skyeparagon@gmail.com>
2023-01-26 15:04:06 +0100

    style: ended loading animation

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -3,3 +3,11 @@ export { default as send } from './send.svg'
 export { default as user } from './user.svg'
 export { default as preview } from './preview.png'
 export { default as download } from './download.png'
+import loadingif from './placeholder.gif'
+import loadingif2 from './placeholder2.gif'
+import loadingif3 from './placeholder3.gif'
+import loadingif4 from './placeholder4.gif'
+import loadingif5 from './placeholder5.gif'
+import loadingif6 from './placeholder6.gif'
+
+export const placeholders = [loadingif, loadingif2, loadingif3, loadingif4, loadingif5, loadingif6]
